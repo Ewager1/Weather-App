e28a47ae7f7658cb4d4c3b872a455ace8f166c36
EWager <ethanbwager@gmail.com>
2020-10-28 11:23:35 -0400

    added appropriate symbols to weather data (like farenheight and MPH)

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// Snapshot Section
+// 1. Add Units to the ends of snapShot Data
+// 2. Find UV
+// 	a. Which API?
+// 	b. figure out the wait ajax function to make UV run at same time as other snapshot data
+// 	c. change UV color based on it's number. Look up favorable, moderate, severe
+// 3. Add date to snaptshot from scheduling app
+
+// 5 Day Section
+// 1. Set up 5 columns or use float left with margins for 5 days
+// 2. Pull Api for Boston and set up default temp and humidity
+// 3. create Time feature that makes each date. (For loop for generating these?)
+// 4. Basic Style
+
+// Search Results Section:
+// 1. Create dynamic div creation with city name that runs api onClick
+// 2. Create Clear Button
+// 3. Create a clear button
+
+// Styling
+// 1. give padding
+// 2. make responsive
+// 3. add favicon
+
 //assignments
 const apiKey = "1222240c823f23e742864fd02f06eb25"; //my Api Key
 let cityInput = $(".cityInput"); // grabs the search input box
@@ -26,9 +50,9 @@ $.ajax({
 
   //creates p elements, adds weather data, then appends to html
   let weatherArray = [
-    `Farenheight: ${temp}`,
-    ` Wind Speed: ${windSpeed}`,
-    `Humidity: ${humidity}`,
+    `fahrenheit: ${temp} \u00B0F`,
+    ` Wind Speed: ${windSpeed} MPH`,
+    `Humidity: ${humidity}%`,
   ];
   for (i = 0; i < weatherArray.length; i++) {
     let pTag = $("<p>");
@@ -64,7 +88,7 @@ inputBtn.on("click", function () {
 
     //updates weather data to user's chosen cit
     weatherArray = [
-      `Farenheight: ${temp}`,
+      `fahrenheit: ${temp}  &#8457`,
       ` Wind Speed: ${windSpeed}`,
       `Humidity: ${humidity}`,
     ];
